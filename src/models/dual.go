dce9edfb8ff0999ed8ad80429a17c68b85a5009f
2heunxun <seapeon@naver.com>
2026-05-16 20:20:42 +0900

    GORM dev

--- src/models/dual.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Dual struct {
-    Username string `gorm:"column:username;primaryKey;size:32" json:"username"`
-    DualRels []DualRel `gorm:"foreignKey:Username" json:"-"`
-    DualRels []DualRel `gorm:"foreignKey:CheckerUsername" json:"-"`
-}
-
-func (Dual) TableName() string { return "dual" }
